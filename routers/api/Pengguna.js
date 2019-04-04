69b7404473f0de3f8cd4174f674dea197c0ae84d
Arkoes07 <m.alwi@ui.ac.id>
2019-04-04 11:07:22 +0700

    login dan signup api

--- routers/api/Pengguna.js
+++ routers/api/Pengguna.js
@@ -1,6 +1,7 @@
+const bcrypt = require('bcrypt')
 const Router = require('../Router')
 
-class Pos extends Router {
+class Pengguna extends Router {
 
     getServices() {
         return {
@@ -9,14 +10,56 @@ class Pos extends Router {
         }
     }
 
-    userLogin(req,res) {
-        res.json({msg: 'user login proced' });
+    userLogin(req, res) {
+        const { username, password } = req.body
+        const text = "SELECT * FROM Pengguna WHERE username = $1"
+        const value = [username]
+        this.client.query(text, value, (err, result) => {
+            if(err){
+                return res.status(400).json({ err })
+            }else{
+                if(!result.rows[0]){
+                    return res.status(400).json({ err : "Pengguna tidak ditemukan" })
+                }else{
+                    bcrypt.compare(password, result.rows[0].password, (err, same) => {
+                        if(err){
+                            return res.status(400).json({ err })
+                        }else{
+                            if(!same){
+                                return res.json({ err : "Password Salah" })
+                            }else{
+                                res.json({ msg : "berhasil login"})
+                            }
+                        }
+                    })
+                }
+            }
+        })
     }
 
-    userSignup(req,res) {
-        res.json({ msg: 'user signed up' });
+    userSignup(req, res) {
+        const { username, password, jenisUser } = req.body
+        const  usernameRegex = /^[a-zA-Z][\w_.]+$/
+        const result = username.match(usernameRegex)
+        if(result == null){
+            return res.status(400).json({ err : "username tidak sesuai aturan" })
+        }
+        const saltRounds = 10;
+        bcrypt.hash(password, saltRounds, (err, enc) => {
+            if(err){
+                return res.status(400).json({ err })
+            }
+            const text = 'INSERT INTO Pengguna values ($1,$2,$3) RETURNING *'
+            const values = [username, enc, jenisUser]
+            this.client.query(text, values, (err, result) => {
+                if(err){
+                    return res.status(400).json({ err })
+                }else{
+                    res.json(result.rows)
+                }
+            })
+        })
     }
-
 }
 
-module.exports = Pos;
+module.exports = Pengguna;
